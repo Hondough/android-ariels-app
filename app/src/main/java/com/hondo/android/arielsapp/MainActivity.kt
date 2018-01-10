cc24c44ea829c162c05f6a1f0d32ee85a771f49b
John Hannemann <Hondough@users.noreply.github.com>
2018-01-10 08:51:27 -0600

    Clean up Code: MainActivity.kt - cleaned up imports and reformatted some code

--- app/src/main/java/com/hondo/android/arielsapp/MainActivity.kt
+++ app/src/main/java/com/hondo/android/arielsapp/MainActivity.kt
@@ -1,16 +1,15 @@
 package com.hondo.android.arielsapp
 
-import android.net.Uri
-import android.os.Bundle
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.net.Uri
+import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.View
 import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 
@@ -33,9 +32,11 @@ class MainActivity : Activity() {
     }
 
     fun updatePreferences(v: View) {
-        val intent = Intent(this, Options::class.java)
         v.invalidate()
-        startActivityForResult(intent, 0)
+        startActivityForResult(
+                Intent(this, Options::class.java),
+                0
+        )
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
